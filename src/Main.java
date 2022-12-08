7ab3309ca716432e22f33875c14694b01fbc21ff
olenikov <a.olenikov@me.com>
2022-12-08 19:44:29 +0300

    6task

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i = 1904; i <= 2096; i = i + 4){
-            System.out.println( i + " год является високосным");
+        for(int i = 7; i <= 98; i = i + 7){
+            System.out.println( i );
         }
     }
 }
