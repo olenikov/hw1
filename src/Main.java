ff6f911226fc14661cd26795c6bfef4177895826
olenikov <a.olenikov@me.com>
2022-12-08 20:02:10 +0300

    8task

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i = 7; i <= 98; i = i + 7){
-            System.out.println( i );
+        int salary = 29000;
+        int total = 0;
+        int mounth = 0;
+        for (int i = 0; i < 12; i++) {
+            total = total + salary;
+            mounth = mounth + 1;
+            System.out.println("Месяц " + mounth + " сумма накоплений равна " + total + " рублей");
+        }
+
         }
     }
-}
