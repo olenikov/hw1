68b693b287b4e3f843b9e56e253b1445320467bf
olenikov <a.olenikov@me.com>
2022-12-08 19:35:45 +0300

    2task

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i = 0; i <= 10; i = i + 1){
+        for(int i = 10; i >= 1; i = i - 1){
             System.out.println("Итерация цикла " + i);
         }
     }
