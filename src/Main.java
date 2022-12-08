97af35638e6599bb08b13963c93e1499dad3b575
olenikov <a.olenikov@me.com>
2022-12-08 19:45:17 +0300

    7task

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i = 7; i <= 98; i = i + 7){
+        for(int i = 1; i <= 256; i = i * 2){
             System.out.println( i );
         }
     }
