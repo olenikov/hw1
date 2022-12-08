09de87c98a4ec9e9b26bc0d0202cd03138cc7e19
olenikov <a.olenikov@me.com>
2022-12-08 20:19:04 +0300

    10task

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int salary = 29000;
-        double total = 0;
-        int mounth = 0;
-        for (int i = 0; i < 12; i++) {
-            total = ( total + salary ) * 1.01;
-            mounth = mounth + 1;
-            System.out.println("Месяц " + mounth + " сумма накоплений равна " + total + " рублей");
+
+        int ab = 1;
+        for (int a = 1; a <= 10; a++) {
+                       ab = 2 * a ;
+            System.out.println("2*" + a + "=" + ab);
         }
 
         }
